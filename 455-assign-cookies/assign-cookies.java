507fe2a4288ad8ce2d597d1cdddfa73b99cb88f5
Saksham Gupta <rgupta69450@gmail.com>
2026-05-16 00:51:14 +0530

    Time: 14 ms (94.31%) | Memory: 51.5 MB (55.41%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int n=g.length,m=s.length,l=0,r=0;
+        while(l<m&&r<n){
+           if(g[r]<=s[l]){
+             r++;
+           }
+           l++;
+        }
+        return r;
+    }
+}
