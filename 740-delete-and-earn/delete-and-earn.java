bc7213d460fd63fb6a5eb0fbd190f59b26fdef13
Saksham Gupta <rgupta69450@gmail.com>
2026-09-01 23:45:03 +0530

    Time: 8 ms (31.91%) | Memory: 47.8 MB (7.26%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -7,16 +7,18 @@ class Solution {
             freq[nums[i]]++;
         }
         if(nums.length==1)return nums[0];
-        return max(freq,0,10001,dp);
+        return max(freq,10001,dp);
         
     }
-    private int max(int []arr,int i,int n,int dp[]){
-        if(i==n-1)return arr[i];
-        if(i>=n)return 0;
-
-        if(dp[i]!=-1){  return dp[i];}
-        int take=arr[i]*i+max(arr,i+2,n,dp);
-        int notTake=max(arr,i+1,n,dp);
-        return dp[i]=Math.max(take,notTake);
+    private int max(int []arr,int n,int dp[]){
+       dp[0]=arr[0];
+       dp[1]=arr[1];
+       int i;
+        for(i=1;i<n;i++){
+            if(i-2>=0){
+            dp[i]=Math.max(arr[i]*i+dp[i-2],dp[i-1]);
+            }
+        }
+        return dp[i-1];
     }
 }
