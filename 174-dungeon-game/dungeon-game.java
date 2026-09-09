f30030abbb74b1f9b360e2b3f071e6aac3d1ef71
Saksham Gupta <rgupta69450@gmail.com>
2026-09-09 11:28:49 +0530

    Time: 3 ms (29.34%) | Memory: 45.3 MB (55.51%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -9,20 +9,36 @@ class Solution {
             }
         }
 
-        return min(0,0,n,m,dungeon,dp);
+        return min(n,m,dungeon,dp);
         
     }
-    private  int min(int i,int j,int n,int m,int arr[][],int dp[][]){
-        if(i==n-1&&j==m-1)return arr[i][j]>0?1:-arr[i][j]+1;
-        if(i>=n||j>=m)return Integer.MAX_VALUE;
+    private  int min(int n,int m,int arr[][],int dp[][]){
+        // if(i==n-1&&j==m-1)return arr[i][j]>0?1:-arr[i][j]+1;
+        // if(i>=n||j>=m)return Integer.MAX_VALUE;        
+        // if(dp[i][j]!=-1)return dp[i][j];
 
-        
-        if(dp[i][j]!=-1)return dp[i][j];
+        // int r=min(i,j+1,n,m,arr,dp);
+        // int d=min(i+1,j,n,m,arr,dp);
+        // int res=Math.min(r,d)-arr[i][j];
 
-        int r=min(i,j+1,n,m,arr,dp);
-        int d=min(i+1,j,n,m,arr,dp);
-        int res=Math.min(r,d)-arr[i][j];
+        // return  dp[i][j]=Math.max(1,res);
+        if(arr[n-1][m-1]>0)dp[n-1][m-1]=1;
+        else
+        dp[n-1][m-1]=-arr[n-1][m-1]+1;
 
-        return  dp[i][j]=Math.max(1,res);
+        for (int j=m-2;j>=0;j--){
+         dp[n-1][j]=Math.max(1,dp[n-1][j+1]-arr[n-1][j]);
+          }
+         for (int j=n-2;j>=0;j--){
+         dp[j][m-1]=Math.max(1,dp[j+1][m-1]-arr[j][m-1]);
+          }
+
+          for(int i=n-2;i>=0;i--){
+            for(int j=m-2;j>=0;j--){
+                int need=Math.min(dp[i+1][j],dp[i][j+1])-arr[i][j];
+                dp[i][j]=Math.max(1,need);
+            }
+          }
+          return dp[0][0];
     }
 }
