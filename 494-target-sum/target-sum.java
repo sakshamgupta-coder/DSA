c8914d0ed652be4c388f0ba48b18ef9eed57187c
Saksham Gupta <rgupta69450@gmail.com>
2026-09-12 09:49:14 +0530

    Time: 177 ms (33.74%) | Memory: 46.4 MB (32.11%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,6 +1,15 @@
 class Solution {
+    static int dp[][];
     public int findTargetSumWays(int[] nums, int target) {
        int n=nums.length;
+       int sum=0;
+       for(int a:nums){
+        sum+=a;
+       }
+       dp=new int[n+1][sum+1];
+       for(int i=0;i<=n;i++){
+        Arrays.fill(dp[i],-1);
+       }
        return ways(n-1,0,target,nums); 
     }
 private int ways(int n,int currSum, int tar,int[] arr){
@@ -8,11 +17,13 @@ private int ways(int n,int currSum, int tar,int[] arr){
     if(tar==currSum)return 1;
     else return 0;
     }
+    if(currSum>=0&&dp[n][currSum]!=-1)return dp[n][currSum];
      int add=0;
      int minus=0;
         minus=ways(n-1,currSum-arr[n],tar,arr);
         add=ways(n-1,currSum+arr[n],tar,arr);
-
-    return minus+add;
-}
+        if(currSum>=0)
+        dp[n][currSum]= minus+add;
+    return  minus+add;
+        }
 }
