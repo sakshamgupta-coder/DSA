da8d24951dd8948226bde720beb5bb853e58a542
Saksham Gupta <rgupta69450@gmail.com>
2026-09-18 22:45:08 +0530

    Time: 5 ms (31.44%) | Memory: 43.7 MB (32.77%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.java
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.java
@@ -6,24 +6,27 @@ class Solution {
         for(int i=0;i<n;i++){
             sum+=arr[i];
         }
-        int  dp[][]=new int [n+1][sum+1];
-        for(int i=0;i<=n;i++){
-            Arrays.fill(dp[i],-1);
-        }
-       return minSum(n-1,sum,0,arr,dp); 
+        int  dp[][]=new int [n+1][sum/2+1];
+       return minSum(n,sum,0,arr,dp); 
         
       }
        private int  minSum(int n,int sum,int curr,int arr[],int dp[][]){
-      if(sum==curr) return 0;
-      if(n==0)return Math.abs(sum-2*curr);
-      
-     if(dp[n][curr]!=-1)return dp[n][curr];
-      
-      int take=minSum(n-1,sum,curr+arr[n],arr,dp);
-      int not=minSum(n-1,sum,curr,arr,dp);
-      
-      
-      return dp[n][curr]=Math.min(take,not);
+    //   if(sum==curr) return 0;
+    //   if(n==0)return Math.abs(sum-2*curr);
+    //  if(dp[n][curr]!=-1)return dp[n][curr]; 
+    //   int take=minSum(n-1,sum,curr+arr[n],arr,dp);
+    //   int not=minSum(n-1,sum,curr,arr,dp);
+    //   return dp[n][curr]=Math.min(take,not);
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=sum/2;j++){
+                if (arr[i-1]<=j){
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j-arr[i-1]]+arr[i-1]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+   return sum-2*dp[n][sum/2];
         
     }
    }
